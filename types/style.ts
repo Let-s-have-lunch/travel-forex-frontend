c16fb88f7d896fcc9b97dedfb300e1fc00f60333
yeonhan <yeonhanlab@gmail.com>
2026-08-18 15:25:57 +0900

    인풋 컴포넌트 작성중

---
+++ types/style.ts
@@ -0,0 +1,10 @@
+export type StyleColorType = "primary" | "secondary" | "success" | "error" | "warning" | "info" | "text";
+export type StyleVariantType = "contained-square" | "contained-circle" | "icon-only" | "outlined";
+export type StyleSizeType = "small" | "medium" | "large";
+
+export const INPUT_SIZE_STYLE = {
+    mini: "text-xs px-1 py-1",
+    small: "text-sm px-4 py-4",
+    medium: "text-lg px-5 py-5",
+    large: "text-xl px-8 py-8",
+};
